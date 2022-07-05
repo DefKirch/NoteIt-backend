e2d4bba64765b26e431d2cc679e17cc9790e829f
DEFKIRCH <thijmen.kirch@gmail.com>
2022-07-05 22:34:32 +0200

    Added seeds for the tasks

--- routers/projects.js
+++ routers/projects.js
@@ -1,5 +1,6 @@
 const { Router } = require("express");
 const User = require("../models/").user;
+const Task = require("../models/").task;
 const Project = require("../models").project;
 const router = new Router();
 
@@ -9,6 +10,19 @@ router.get("/test", async (req, res) => {
   });
   console.log("userTest:", userTest);
   res.status(200).send(userTest);
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const projectResponse = await Project.findByPk(id, {
+      include: [User, Task],
+    });
+    res.status(200).send(projectResponse);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
 });
 
 module.exports = router;
---
+++ seeders/20220705202430-some-tasks.js
@@ -0,0 +1,72 @@
+"use strict";
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      "tasks",
+      [
+        {
+          title: "Make a list",
+          description: "",
+          status: "Todo",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 1,
+        },
+        {
+          title: "Try some new things",
+          description:
+            "Look at this and this to discover new things to try on this project",
+          status: "In Progress",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 1,
+        },
+        {
+          title: "Draw something",
+          description: "Draw a picture of something, just something",
+          status: "Todo",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 2,
+        },
+        {
+          title: "Make a new playlist",
+          description: "Check out that new artist, or not...",
+          status: "Done",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 1,
+        },
+        {
+          title: "Pet the DBeaver",
+          description:
+            "what? pet the dbeaver? but that's a databasemanager, right?",
+          status: "Todo",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 3,
+        },
+        {
+          title: "Setup a migration",
+          description:
+            "migrate some things, model some things and seed them I guess",
+          status: "In Progress",
+          createdAt: new Date(),
+          updatedAt: new Date(),
+          projectId: 2,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  },
+};
