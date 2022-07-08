af6fe78ea9a6166c7540e2b56961bf5707bdd649
DEFKIRCH <thijmen.kirch@gmail.com>
2022-07-08 10:28:40 +0200

    Setup route for projectOverviewPage

--- routers/projects.js
+++ routers/projects.js
@@ -1,15 +1,28 @@
 const { Router } = require("express");
+const { toData } = require("../auth/jwt");
+const authMiddleware = require("../auth/middleware");
 const User = require("../models/").user;
 const Task = require("../models/").task;
 const Project = require("../models").project;
 const router = new Router();
 
-router.get("/test", async (req, res) => {
-  const userTest = await User.findByPk(2, {
-    include: [{ model: Project, include: [User] }],
-  });
-  console.log("userTest:", userTest);
-  res.status(200).send(userTest);
+router.get("/me", authMiddleware, async (req, res, next) => {
+  try {
+    const auth =
+      req.headers.authorization && req.headers.authorization.split(" ");
+    const data = toData(auth[1]);
+    const id = data.userId;
+    console.log("ID", id);
+    const userProjects = await User.findByPk(id, {
+      include: [{ model: Project, include: [User] }],
+    });
+    // delete userProjects.dataValues["password"];
+    console.log(userProjects);
+    res.status(200).send(userProjects);
+  } catch (e) {
+    console.log(e.message);
+    next(e);
+  }
 });
 
 router.get("/:id", async (req, res, next) => {
@@ -18,6 +31,8 @@ router.get("/:id", async (req, res, next) => {
     const projectResponse = await Project.findByPk(id, {
       include: [User, Task],
     });
+    // Todo: remove some of the details of the other users and all passwords when sending back the information
+    // delete projectResponse.dataValues["password"];
     res.status(200).send(projectResponse);
   } catch (e) {
     console.log(e.message);
